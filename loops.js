4cab0dced3ba1fd225f008c726fb2cb6dea04fb3
joanneelizabethhooton@gmail.com <joanneelizabethhooton@gmail.com>
2018-07-16 23:21:12 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,15 +1,21 @@
-function forLoop(array){
-  for(var i = 0; i <25; i++){
+function forLoop(array) {
+  for (var i = 0; i <25; i++) {
     if (i === 1){
-      array.push(`I am ${i} strange loop.`)
+      array.push (`I am ${i} strange loop.`)
     } else {
-      array.push(`I am ${i} strange loops.`)
+      array.push (`I am ${i} strange loops.`)
   }
 }
 return array
 }
-function whileLoop(n){
+function whileLoop(n) {
   while(n>0)
   {console.log(n--)}
   return "done"
+}
+function doWhileLoop(array) {
+do {
+array.pop()
+} while (array.length>0 && maybeTrue())
+return array
 }
