13179020154dc7c00ae77761de463bcd135ee327
joanneelizabethhooton@gmail.com <joanneelizabethhooton@gmail.com>
2018-07-16 23:23:00 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,9 +16,9 @@ function whileLoop(n) {
 function maybeTrue() {
   return Math.random() >= 0.5
 }
-function doWhileLoop(array){
+function doWhileLoop(array) {
 do {
   array.pop()
-} while (array.length>0 && maybeTrue())
+} while (array.length >0 && maybeTrue())
 return array
 }
