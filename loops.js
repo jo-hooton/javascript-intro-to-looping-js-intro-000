38fa16613fca29e149f6a6e4b11d457e4889c14f
joanneelizabethhooton@gmail.com <joanneelizabethhooton@gmail.com>
2018-07-16 22:53:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 for (let i = 0; i < 25; i++) {
-    if i = 0 { "I am a strange loop.";
-  } else if i = 1 { "I am "+i+" strange loop";
-} else { "I am "+${i}+" strange loops.";
+    if i = 0 { ("I am a strange loop.");
+  } else if i = 1 { ("I am "+i+" strange loop");
+} else { ("I am "+${i}+" strange loops.");
   }
 }
